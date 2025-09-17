0d1ab714ae6a3ac311494367b4bbcd94540445cf
Philip <54486671+Marm0s@users.noreply.github.com>
2025-09-17 07:47:50 +0200

    Add files via upload

---
+++ StopWatch/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace StopWatch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly DispatcherTimer dispatcherTimer = new();
+        Stopwatch stopWatch = new();
+        string currentTime = string.Empty;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dispatcherTimer.Tick += new EventHandler(timer_tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+        }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            if (stopWatch.IsRunning)
+            {
+                TimeSpan ts = stopWatch.Elapsed;
+                currentTime = String.Format("{0:00}:{1:00}:{2:00}",
+                ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+                Clock.Content = currentTime;
+            }
+        }
+
+
+        private void Button_Start(object sender, RoutedEventArgs e)
+        {
+            stopWatch.Start();
+            dispatcherTimer.Start();
+        }
+
+        private void Button_Stop(object sender, RoutedEventArgs e)
+        {
+            if (stopWatch.IsRunning)
+            {
+                stopWatch.Stop();
+            }
+            Clock.Content = currentTime;
+            elapsedtimeitem.Items.Add(currentTime);
+        }
+
+        private void Button_Reset(object sender, RoutedEventArgs e)
+        {
+           stopWatch.Reset();
+           Clock.Content = "00:00:00";
+        }
+    }
+}
